b3a55d7efc6a30b704c818afcf5f8748440b5a14
Hemendra Singh <85049926+Hemendra-S27@users.noreply.github.com>
2023-07-08 15:21:38 +0530

    Update cpumon.py
    d

--- cpumon.py
+++ cpumon.py
@@ -1,27 +1,33 @@
-import os
+import tkinter as tk
+from tkinter import ttk
 import psutil
 import time
-from termcolor import colored
+import threading
 
-def print_cpu_utilization():
-    cpu_utilization = psutil.cpu_times_percent(interval=1)
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("CPU Monitor")
+        
+        self.cpus = psutil.cpu_count()
+        self.cpu_vars = [tk.DoubleVar(value=0) for _ in range(self.cpus)]
+        self.progress_bars = []
 
-    sys_utilization = cpu_utilization.system
-    user_utilization = cpu_utilization.user
-    idle_utilization = cpu_utilization.idle
+        for i in range(self.cpus):
+            ttk.Label(self, text=f"CPU {i+1}").grid(column=0, row=i)
+            pb = ttk.Progressbar(self, maximum=100, length=300, variable=self.cpu_vars[i])
+            pb.grid(column=1, row=i, padx=10, pady=5)
+            self.progress_bars.append(pb)
 
-    print(colored("System Utilization", "red"))
-    print(colored("|" * int(sys_utilization), "red"))
+        self.update_cpu_usage()
 
-    print(colored("User Utilization", "blue"))
-    print(colored("|" * int(user_utilization), "blue"))
+    def update_cpu_usage(self):
+        cpu_percentages = psutil.cpu_percent(percpu=True)
+        for i in range(self.cpus):
+            self.cpu_vars[i].set(cpu_percentages[i])
+        self.after(1000, self.update_cpu_usage)
 
-    print(colored("Idle Utilization", "green"))
-    print(colored("|" * int(idle_utilization), "green"))
 
-    print("\n")
-
-while True:
-    os.system('clear')
-    print_cpu_utilization()
-    time.sleep(1)
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
